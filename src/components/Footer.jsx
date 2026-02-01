759883187d895f4dab54f8bad0af33492c6fb2ba
fachri <fahriqyu01@gmail.com>
2026-02-01 21:10:39 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+ import '../styles/Footer.css'
+ 
+ function Footer() {
+   return (
+      <div id="copyright">
+        <div className="wrapper">
+            &copy; 2024. <b>RumahJawa.</b> All Rights Reserved.
+        </div>
+    </div>
+   )
+ }
+ 
+ export default Footer
+ 
---
+++ src/data/ContactSection.jsx
@@ -0,0 +1,20 @@
+export const contactSection = [
+    {
+        content : `<h3>Muhammad Fachri Muthifatih.</h3>
+                    <p>Moto Hidup Saya : Your life is a good as your mindset!</p>`
+    },
+     {
+        content : `<h3>About</h3>
+                    <p>Saya Mahasiswa STTP tahun 2024-2025!, dari Prodi Informatika</p>`
+    },
+     {
+        content : ` <h3>Contact</h3>
+                    <p>085879768010</p>
+                    <p>Ds. Tayu Wetan, RT/RW 02/02 Kec. Tayu, Kab. Pati, Jawa Tengah</p>
+                    <p>Kode Pos: 59156</p>`
+    },
+     {
+        content : ` <h3>Social</h3>
+                    <p><b>Instagram </b>mfachrii_</p>`
+    },
+]
